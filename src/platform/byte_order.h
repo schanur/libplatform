3c58a223fec8c741a23cd255f234957bd89d957e
Bjoern Griebenow <b.griebenow@web.de>
2014-06-26 18:54:05 +0200

    BYTE_ORDER is now defined by the C lib. Renamed all "BYTE_ORDER" to "PLATFORM_BYTE_ORDER".

--- src/platform/byte_order.h
+++ src/platform/byte_order.h
@@ -1,44 +1,42 @@
 #ifndef PLATFORM_BYTE_ORDER_H
-#define PLATFORM_BYTE_ORDER_H
+  #define PLATFORM_BYTE_ORDER_H
 
-#ifdef BYTE_ORDER 
-#error Redefinition of BYTE_ORDER 
-#endif
-#if __BYTE_ORDER == __BIG_ENDIAN
-#define BYTE_ORDER BIG_ENDIAN
-#else
-#define BYTE_ORDER LITTLE_ENDIAN
-#endif
-
-
-#include "os_detect.h"
-#if BYTE_ORDER == LITTLE_ENDIAN
-#include <byteswap.h>
-#endif
+  #ifdef PLATFORM_BYTE_ORDER 
+    #error Redefinition of PLATFORM_BYTE_ORDER 
+  #endif
+  #if __BYTE_ORDER == __BIG_ENDIAN
+    #define PLATFORM_BYTE_ORDER BIG_ENDIAN
+  #else
+    #define PLATFORM_BYTE_ORDER LITTLE_ENDIAN
+  #endif
 
 
-#ifdef PLATFORM_LINUX
-
-#include <netinet/in.h>
-#define NTOH_16(x) (ntohs(x))
-#define HTON_16(x) (htons(x))
-#define NTOH_32(x) (ntohl(x))
-#define HTON_32(x) (htonl(x))
-#if BYTE_ORDER == BIG_ENDIAN
-#define NTOH_64(x) (x)
-#define HTON_64(x) (x)
-#else
-#define NTOH_64(x) (bswap_64(x))
-#define HTON_64(x) (bswap_64(x))
-#endif
-
-#else
-#ifdef PLATFORM_WINDOWS
-
-#else
-#error No platform defined.
-#endif
-#endif
+  #include "os_detect.h"
+  #if PLATFORM_BYTE_ORDER == LITTLE_ENDIAN
+    #include <byteswap.h>
+  #endif
 
 
+  #ifdef PLATFORM_LINUX
+
+    #include <netinet/in.h>
+    #define NTOH_16(x) (ntohs(x))
+    #define HTON_16(x) (htons(x))
+    #define NTOH_32(x) (ntohl(x))
+    #define HTON_32(x) (htonl(x))
+    #if PLATFORM_BYTE_ORDER == BIG_ENDIAN
+      #define NTOH_64(x) (x)
+      #define HTON_64(x) (x)
+    #else
+      #define NTOH_64(x) (bswap_64(x))
+      #define HTON_64(x) (bswap_64(x))
+    #endif
+
+  #else
+    #ifdef PLATFORM_WINDOWS
+
+    #else
+      #error No platform defined.
+    #endif
+  #endif
 #endif /* PLATFORM_BYTE_ORDER_H */
