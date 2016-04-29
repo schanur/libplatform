5ed06fce4fdcbac7f410372cd42ade0e35025318
Bjoern Griebenow <b.griebenow@web.de>
2016-04-29 19:36:14 +0200

    Add missing headers to all.h

--- src/platform/all.h
+++ src/platform/all.h
@@ -1,14 +1,22 @@
 #ifndef PLATFORM_ALL_H
 #define PLATFORM_ALL_H
 
-#include "os_detect.h"
+#include "assert_wrapper.h"
+#include "byte_order.h"
 #include "compiler.h"
-
-#include "thread.h"
-#include "shm.h"
-#include "mutex.h"
+#include "compliance.h"
+#include "condition.h"
 #include "dynlib.h"
+#include "interprocess_mutex.h"
+#include "inttypes_wrapper.h"
+#include "mem.h"
+#include "mutex.h"
+#include "os_detect.h"
+#include "shm.h"
 #include "socket.h"
+#include "system_error.h"
+#include "thread.h"
 #include "time.h"
+#include "type_size.h"
 
 #endif /* PLATFORM_ALL_H */
