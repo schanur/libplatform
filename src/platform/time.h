dc77f344761059fa8b313086497b933b6f5d9830
Bjoern Griebenow <b.griebenow@web.de>
2018-08-12 21:19:42 +0200

    Add sleep function with granularity better than sec have a *__CHECK version
    The *__CHECK versions use the error code and calculate the remaining
    time if the sleep call was interrupted.

--- src/platform/time.h
+++ src/platform/time.h
@@ -3,37 +3,93 @@
 
 #include "os_detect.h"
 
+#include "assertion.h"
+
 #ifdef PLATFORM_LINUX
-#include <unistd.h>
-#define SLEEP_SEC(seconds)             sleep(seconds);
-#define SLEEP_MSEC(milliseconds)       usleep(1000 * milliseconds)
-#define CLOCK_GETTIME(clk_id,time_ptr) clock_gettime(clk_id, time_ptr)
+  #include <unistd.h> /* sleep() */
+
+  #include <time.h>   /* nanosleep() */
+
+
+  #ifdef _SOURCE_POSIX
+
+    /* Currently only supported on Unix like systems. The currently
+     * highest supported time resolution on Windows implemented in this
+     * library is milliseconds.
+     */
+    #define PL_PRIVATE_SLEEP_NSEC_CHECK(err,remaining,time,factor)        \
+      do {                                                                \
+          long            nanoseconds;                                    \
+          struct timespec req, rem;                                       \
+          ASSERT_RT(time >= 0);                                           \
+          nanoseconds = time * factor;                                    \
+          req.tv_sec  = ((time_t) nanoseconds) / 1000000000;              \
+          req.tv_nsec = ((long)   nanoseconds) % 1000000000;              \
+          err         = nanosleep(&req , &rem);                           \
+          remaining   = (rem.tv_sec * 1000000000 + rem.tv_nsec) / factor; \
+      } while (0);
+
+    #define PL_PRIVATE_SLEEP_NSEC(nanoseconds)                            \
+      do {                                                                \
+          struct timespec req, rem;                                       \
+          ASSERT_RT(nanoseconds >= 0);                                    \
+          req.tv_sec  = ((time_t) nanoseconds) / 1000000000;              \
+          req.tv_nsec = ((long)   nanoseconds) % 1000000000;              \
+          (void) nanosleep(&req , &rem);                                  \
+      } while (0);
+
+    #define SLEEP_NSEC_CHECK(err,remaining,nanoseconds) PL_PRIVATE_SLEEP_NSEC_CHECK(err, remaining, nanoseconds, 1)
+    #define SLEEP_NSEC(nanoseconds) PL_PRIVATE_SLEEP_NSEC(nanoseconds)
+
+    /* Usleep is deprecated in newer Posix versions. We use nanosleep
+     * instead. TODO: We can use usleep on older systems.
+     */
+    /* #define SLEEP_MSEC(milliseconds)       usleep(1000 * milliseconds) */
+
+    #define SLEEP_USEC_CHECK(err,remaining,microseconds) PL_PRIVATE_SLEEP_NSEC_CHECK(err, remaining, microseconds, 1000)
+    #define SLEEP_USEC(microseconds)                                      \
+      do {                                                                \
+          ASSERT_RT(microseconds < )
+          SLEEP_NSEC(microseconds * 1000)                                 \
+}     while (0);
+
+    #define SLEEP_MSEC_CHECK(err,remaining,milliseconds) PL_PRIVATE_SLEEP_NSEC_CHECK(err, remaining, milliseconds, 1000000)
+    #define SLEEP_MSEC(milliseconds) SLEEP_NSEC(milliseconds * 1000000)
+
+  #endif /*_SOURCE_POSIX */
+
+
+  #define SLEEP_SEC(seconds)             ASSERT_RT((int) seconds >= 0); sleep(seconds);
+
+  #define CLOCK_GETTIME(clk_id,time_ptr) clock_gettime(clk_id, time_ptr)
 
 #else
-#ifdef PLATFORM_WINDOWS
+  #ifdef PLATFORM_WINDOWS
 
-//#if 0
-#include <windows.h> /* Sleep() */
+  //#if 0
+  #include <windows.h> /* Sleep() */
 
-#define SLEEP_SEC(seconds)             Sleep(seconds * 1000);
-#define SLEEP_MSEC(milliseconds)       Sleep(milliseconds);
-#define CLOCK_GETTIME(clk_id,time_ptr)
-/* typedef long time_t; */
+  #define SLEEP_MSEC(milliseconds)       ASSERT_RT(seconds >= 0); Sleep(milliseconds);
 
-/* struct timespec_t { */
-/*     time_t tv_sec; */
-/*     long   tv_nsec; */
-/* }; */
-/* struct timespec_t { */
-/*     int64_t tv_sec; */
-/*     long    tv_nsec; */
-/* }; */
-//#endif
+  #define SLEEP_SEC(seconds)             ASSERT_RT(seconds >= 0); Sleep(seconds * 1000);
+
+  #define CLOCK_GETTIME(clk_id,time_ptr)
+  /* typedef long time_t; */
+
+  /* struct timespec_t { */
+  /*     time_t tv_sec; */
+  /*     long   tv_nsec; */
+  /* }; */
+  /* struct timespec_t { */
+  /*     int64_t tv_sec; */
+  /*     long    tv_nsec; */
+  /* }; */
+  //#endif
 
 
-#else
-#error No platform defined.
-#endif
+  #else
+    #error No platform defined.
+  #endif
 #endif
 
 
--- test/module/time.c
+++ test/module/time.c
@@ -3,20 +3,71 @@
 #include "platform/time.h"
 
 
-static void test__byte_order__SLEEP_SEC()
+static void test__time__SLEEP_SEC()
 {
     SLEEP_SEC(1);
 }
 
-static void test__byte_order__SLEEP_MSEC()
+
+#ifdef _SOURCE_POSIX
+static void test__time__SLEEP_MSEC()
 {
+    SLEEP_MSEC(1);
+
+}
+
+static void test__time__SLEEP_USEC()
+{
+    SLEEP_USEC(1);
+}
+
+
+static void test__time__SLEEP_NSEC()
+{
+    SLEEP_NSEC(1);
+}
+
+
+static void test__time__SLEEP_MSEC_CHECK()
+{
+    int     err;
+    int64_t remaining;
+    int64_t time = 1;
     SLEEP_MSEC(1);
 }
 
+static void test__time__SLEEP_USEC_CHECK()
+{
+    int     err;
+    int64_t remaining;
+    int64_t time = 1;
+    SLEEP_USEC(1);
+}
+
+static void test__time__SLEEP_NSEC_CHECK()
+{
+    int     err;
+    int64_t remaining;
+    int64_t time = 1;
+    SLEEP_NSEC(1);
+}
+
+#endif
+
+
 int main(void)
 {
-    test__byte_order__SLEEP_SEC();
-    test__byte_order__SLEEP_MSEC();
+    test__time__SLEEP_SEC();
+
+#ifdef _SOURCE_POSIX
+    test__time__SLEEP_MSEC();
+    test__time__SLEEP_USEC();
+    test__time__SLEEP_NSEC();
+
+    test__time__SLEEP_MSEC_CHECK();
+    test__time__SLEEP_USEC_CHECK();
+    test__time__SLEEP_NSEC_CHECK();
+#endif
 
     return 0;
 }
