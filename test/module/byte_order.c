e7c0aad370eab325b695d402d612f2926c1edc41
Bjoern Griebenow <b.griebenow@web.de>
2019-06-27 16:34:35 +0200

    Add tests for byte_order and mem to assure required symbols got defined

--- test/module/byte_order.c
+++ test/module/byte_order.c
@@ -3,6 +3,35 @@
 #include "platform/byte_order.h"
 
 
+#ifndef PLATFORM_BYTE_ORDER
+  #error PLATFORM_BYTE_ORDER not defined
+#endif
+
+#ifndef NTOH_16
+  #error NTOH_16 not defined
+#endif
+
+#ifndef NTOH_32
+  #error NTOH_32 not defined
+#endif
+
+#ifndef NTOH_64
+  #error NTOH_64 not defined
+#endif
+
+#ifndef HTON_16
+  #error HTON_16 not defined
+#endif
+
+#ifndef HTON_32
+  #error HTON_32 not defined
+#endif
+
+#ifndef HTON_64
+  #error HTON_64 not defined
+#endif
+
+
 static void test__byte_order__()
 {
 
--- test/module/mem.c
+++ test/module/mem.c
@@ -3,6 +3,30 @@
 #include "platform/mem.h"
 
 
+#ifndef MEM_PAGE_SIZE
+  #error MEM_PAGE_SIZE not defined
+#endif
+
+#ifndef MEM_READ
+  #error MEM_READ not defined
+#endif
+
+#ifndef MEM_WRITE
+  #error MEM_WRITE not defined
+#endif
+
+#ifndef MEM_EXEC
+  #error MEM_EXEC not defined
+#endif
+
+#ifndef MEM_ALLOC_ANON_PAGE
+  #error MEM_ALLOC_ANON_PAGE not defined
+#endif
+
+#ifndef MEM_FREE_ANON_PAGE
+  #error MEM_FREE_ANON_PAGE not defined
+#endif
+
 
 int main(void)
 {
