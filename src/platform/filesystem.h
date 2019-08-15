788e9247315fcad58bcf4d8f4e9bce59c8f0adef
Bjoern Griebenow <b.griebenow@web.de>
2019-08-16 01:17:13 +0200

    Add multiple macros in filesystem header

--- src/platform/filesystem.h
+++ src/platform/filesystem.h
@@ -9,8 +9,28 @@
   #include <sys/types.h>
   #include <sys/stat.h>
 
-#define PLTF_MKDIR(err,pathname)     (err = (mkdir(pathname, 0711) != -1))
+  /* #define _POSIX_C_SOURCE 200809L */
+  #include <linux/limits.h> /* PATH_MAX   */
+  #include <limits.h>       /* realpath() */
+  #include <stdlib.h>       /* realpath() */
+  #include <unistd.h>       /* readlink() */
 
+  #define PLTF_PATH_MAX PATH_MAX
+
+  #define PLTF_MKDIR(err,pathname)                                         (err = (mkdir(pathname, 0711) != -1))
+
+  #ifdef _POSIX_SOURCE
+    #define PLTF_ABS_EXECUTABLE_FILENAME(err,filename,filename_buf_size)   (err = (readlink("/proc/self/exe", filename, filename_buf_size) == -1))
+  #endif
+
+  #define PLTF_REALPATH(err,path,resolved_path)                            (err = (realpath(path, resolved_path)) == NULL)
+
+
+/* #define PLTF_CURRENT_WORKING_DIR */
+/* #define PLTF_CHANGE_WORKING_DIR */
+
+
+/* #elif PLATFORM_FREEBSD */
 #else
   #ifdef PLATFORM_WINDOWS
 
--- test/module/filesystem.c
+++ test/module/filesystem.c
@@ -1,16 +1,34 @@
 #include "../shared_c/test.h"
 
 #include "platform/filesystem.h"
+#include <string.h>
 
 
 static void test__filesystem__MKDIR__create_in_dev_shm()
 {
-    int err = -1;
-    PLTF_MKDIR(err, "/dev/shm/libplatform");
-    mu_assert(!err);
+    int pltf_err = -1;
+    PLTF_MKDIR(pltf_err, "/dev/shm/libplatform");
+    mu_assert(!pltf_err);
 }
+
+
+#ifdef _POSIX_SOURCE
+static void test__filesystem__PLTF_ABS_EXECUTABLE_FILENAME()
+{
+    char filename[PLTF_PATH_MAX];
+    int  pltf_err = -1;
+    strcpy(filename, "");
+    PLTF_ABS_EXECUTABLE_FILENAME(pltf_err, filename, PLTF_PATH_MAX);
+    mu_assert(!pltf_err);
+    mu_assert(strlen(filename) > 0);
+}
+#endif
+
 
 int main(void)
 {
     test__filesystem__MKDIR__create_in_dev_shm();
+#ifdef _POSIX_SOURCE
+    test__filesystem__PLTF_ABS_EXECUTABLE_FILENAME();
+#endif
 }
