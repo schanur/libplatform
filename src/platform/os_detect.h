9c5e874caf171a6e18123be6e32c202077d68374
Bjoern Griebenow <b.griebenow@web.de>
2019-10-03 15:12:47 +0200

    Add PLTF_OS and PLTF_OS_TYPE macros
    PLTF_OS will replace PLATFORM_WINDOWS and PLATFORM_LINUX later.

--- src/platform/os_detect.h
+++ src/platform/os_detect.h
@@ -1,18 +1,41 @@
 #ifndef PLATFORM_OS_DETECT_H
 #define PLATFORM_OS_DETECT_H
 
-/* #define PLATFORM_WINDOWS */
 
-#if !defined(PLATFORM_WINDOWS) && !defined(PLATFORM_LINUX)
+#define PLTF_OS_LINUX        10
+#define PLTF_OS_OSX          20
+#define PLTF_OS_WINDOWS      30
+
+#define PLTF_OS_ANDROID      80
+#define PLTF_OS_APPLE_IOS    90
+
+#define PLTF_OS_BEOS         120
+
+
+#define PLTF_OS_TYPE_POSIX   1000
+#define PLTF_OS_TYPE_WINDOWS 1100
+
+
 #if defined(_MSC_VER) || defined(__cygwin__) || defined(__MINGW32__)
-#define PLATFORM_WINDOWS
+  #define PLATFORM_WINDOWS
+  #define PLTF_OS PLTF_OS_WINDOWS
 #else
-#define PLATFORM_LINUX
-#endif
+  #define PLATFORM_LINUX
+  #define PLTF_OS PLTF_OS_LINUX
 #endif
 
 
-#if !defined(PLATFORM_WINDOWS) && !defined(PLATFORM_LINUX)
+#if (PLTF_OS == PLTF_OS_LINUX) || (PLTF_OS == PLTF_OS_OSX)
+#define PLTF_OS_TYPE PLTF_OS_TYPE_POSIX
+#endif
+
+#if PLTF_OS == PLTF_OS_WINDOWS
+  #define PLTF_OS_TYPE PLTF_OS_TYPE_WINDOWS
+#endif
+
+
+
+#ifndef PLTF_OS
 #error No platform detected
 #endif
 
