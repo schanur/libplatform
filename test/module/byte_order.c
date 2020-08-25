a6ae97565b1f92dd20a96047380f23ed4f86b2c5
Bjoern Griebenow <b.griebenow@web.de>
2020-08-25 12:29:46 +0200

    test: add unit tests for byte order macros

--- test/module/byte_order.c
+++ test/module/byte_order.c
@@ -32,14 +32,26 @@
 #endif
 
 
-static void test__byte_order__()
+static void test__byte_order__ntoh()
 {
-
+    mu_assert(NTOH_16(0x3412)             == 0x1234);
+    mu_assert(NTOH_32(0x78563412)         == 0x12345678);
+    mu_assert(NTOH_64(0xf0debc9a78563412) == 0x123456789abcdef0);
 }
+
+
+static void test__byte_order__hton()
+{
+    mu_assert(HTON_16(0x1234)             == 0x3412);
+    mu_assert(HTON_32(0x12345678)         == 0x78563412);
+    mu_assert(HTON_64(0x123456789abcdef0) == 0xf0debc9a78563412);
+}
+
 
 int main(void)
 {
-    test__byte_order__();
+    test__byte_order__ntoh();
+    test__byte_order__hton();
 
     return 0;
 }
