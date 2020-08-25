d7e19958012e14abcca4f3d1d8c22bdedbdced46
Bjoern Griebenow <b.griebenow@web.de>
2020-08-25 12:27:39 +0200

    fix: use C89 style comment syntax in ASSERT_RT macros
    The old version can break on strict C89 compilers.

--- src/platform/assertion.h
+++ src/platform/assertion.h
@@ -44,9 +44,9 @@
   #ifndef NDEBUG
     #ifdef ASSERT_RT__CRASH_ON_ASSERTION_FAIL
       /* #include <cstdlib> */
-      #define ASSERT_RT(expr) if (!(expr)) { ASSERT_RT__CRASH_COMMAND(expr); } // LCOV_EXCL_LINE LCOV_EXCL__BR_LINE
+      #define ASSERT_RT(expr) if (!(expr)) { ASSERT_RT__CRASH_COMMAND(expr); } /* LCOV_EXCL_LINE LCOV_EXCL__BR_LINE */
     #else
-      #define ASSERT_RT(expr) assert(expr) // LCOV_EXCL_LINE LCOV_EXCL_BR_LINE
+      #define ASSERT_RT(expr) assert(expr) /* LCOV_EXCL_LINE LCOV_EXCL_BR_LINE */
     #endif /* #ifdef ASSERT_RT__CRASH_ON_ASSERTION_FAIL */
   #else
     #define ASSERT_RT(expr) /* Assert macro ignored in release build. */
