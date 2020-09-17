123138129860cfb96f25dd220cdde9ffd21c231c
Bjoern Griebenow <b.griebenow@web.de>
2020-09-17 14:26:40 +0200

    test: fix function name spelling

--- test/module/assertion.c
+++ test/module/assertion.c
@@ -3,12 +3,12 @@
 #include "platform/assertion.h"
 
 
-static void test__assert_wrapper__runtime_asertion_can_pass()
+static void test__assert_wrapper__runtime_assertion_can_pass()
 {
     ASSERT_RT(1);
 }
 
-static void test__assert_wrapper__compile_time_asertion_can_pass()
+static void test__assert_wrapper__compile_time_assertion_can_pass()
 {
     ASSERT_CT(1);
 }
@@ -16,8 +16,8 @@ static void test__assert_wrapper__compile_time_asertion_can_pass()
 
 int main(void)
 {
-    test__assert_wrapper__runtime_asertion_can_pass();
-    test__assert_wrapper__compile_time_asertion_can_pass();
+    test__assert_wrapper__runtime_assertion_can_pass();
+    test__assert_wrapper__compile_time_assertion_can_pass();
 
     return 0;
 }
