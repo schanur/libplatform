ed4bca91d28f19c09e8d12ba29463481eb9c6a0d
Bjoern Griebenow <b.griebenow@web.de>
2020-09-19 16:02:47 +0200

    refactor: use PLTF__ prefix for mem macros

--- src/platform/mem.h
+++ src/platform/mem.h
@@ -3,26 +3,26 @@
 
 #include "os_detect.h"
 
-#define MEM_PAGE_SIZE 4096
+#define PLTF__MEM_PAGE_SIZE 4096
 
 #ifdef PLATFORM_LINUX
 
 #include "sys/mman.h"
 
-#define MEM_READ  PROT_READ
-#define MEM_WRITE PROT_WRITE
-#define MEM_EXEC  PROT_EXEC
+#define PLTF__MEM_READ  PROT_READ
+#define PLTF__MEM_WRITE PROT_WRITE
+#define PLTF__MEM_EXEC  PROT_EXEC
 
-#define MEM_ALLOC_ANON_PAGE(page_cnt,mode) (mmap(NULL, page_cnt * MEM_PAGE_SIZE, mode, MAP_ANONYMOUS, -1, 0))
-#define MEM_FREE_ANON_PAGE(ptr) (munmap(ptr, page_cnt * MEM_PAGE_SIZE))
+#define PLTF__MEM_ALLOC_ANON_PAGE(page_cnt,mode) (mmap  (NULL, page_cnt * MEM_PAGE_SIZE, mode, MAP_ANONYMOUS, -1, 0))
+#define PLTF__MEM_FREE_ANON_PAGE(ptr)            (munmap(ptr,  page_cnt * MEM_PAGE_SIZE))
 
 
 #else
 #ifdef PLATFORM_WINDOWS
 
-#define MEM_READ
-#define MEM_WRITE
-#define MEM_EXEC
+#define PLTF__MEM_READ
+#define PLTF__MEM_WRITE
+#define PLTF__MEM_EXEC
 
 #else
 #error No platform defined.
--- test/module/mem.c
+++ test/module/mem.c
@@ -3,29 +3,29 @@
 #include "platform/mem.h"
 
 
-#ifndef MEM_PAGE_SIZE
-  #error MEM_PAGE_SIZE not defined
+#ifndef PLTF__MEM_PAGE_SIZE
+  #error PLTF__MEM_PAGE_SIZE not defined
 #endif
 
-#ifndef MEM_READ
-  #error MEM_READ not defined
+#ifndef PLTF__MEM_READ
+  #error PLTF__MEM_READ not defined
 #endif
 
-#ifndef MEM_WRITE
-  #error MEM_WRITE not defined
+#ifndef PLTF__MEM_WRITE
+  #error PLTF__MEM_WRITE not defined
 #endif
 
-#ifndef MEM_EXEC
-  #error MEM_EXEC not defined
+#ifndef PLTF__MEM_EXEC
+  #error PLTF__MEM_EXEC not defined
 #endif
 
-#ifndef MEM_ALLOC_ANON_PAGE
-  #error MEM_ALLOC_ANON_PAGE not defined
-#endif
+/* #ifndef PLTF__MEM_ALLOC_ANON_PAGE */
+/*   #error PLTF__MEM_ALLOC_ANON_PAGE not defined */
+/* #endif */
 
-#ifndef MEM_FREE_ANON_PAGE
-  #error MEM_FREE_ANON_PAGE not defined
-#endif
+/* #ifndef PLTF__MEM_FREE_ANON_PAGE */
+/*   #error PLTF__MEM_FREE_ANON_PAGE not defined */
+/* #endif */
 
 
 int main(void)
